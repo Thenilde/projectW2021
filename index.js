6ad872cb3a4db5477142d3dbf3df41e39b2a7478
Thenikde <thenilde.tb@hotmail.com>
2021-03-04 21:45:23 +0000

    change3

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
  const http = require('http');
-    const axios = require('axios');
+ const axios = require('axios');
 
-    http.createServer((req, res)=>{
-      res.write(users.join("\n")); //display the list of users on the page
+http.createServer((req, res)=>{
+//res.write(users.join("\n")); //display the list of users on the page
 
-     res.write("\n\n" + emails.join(","));
+//res.write("\n\n" + emails.join(","));
+
+res.write(chars.join("\n"));
     //users on the page
-    res.end(); //end the response
-    }).listen(8000); // listen for requests on port 8000
+res.end(); //end the response
+ }).listen(8000); // listen for requests on port 8000
 
-   let users = []; // names of users will be stored here
-   let email=[];
+//let users = []; // names of users will be stored here
+//let email=[];
+let chars=[];
 (async function getNames(){
   try{
-    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
-    users = data.map(user=>user.name);
-    emails=data.map(email=>email.email);
-    console.log(users);
-    console.log(emails);
+    //const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+    //users = data.map(user=>user.name);
+    //emails=data.map(email=>email.email);
+
+    const{data}=await axios.get("https://swapi.dev/api/people/");
+    //characters=data.map()
+    //console.log(users);
+    //console.log(emails);
+   
+    chars=data.results.map(char=>char.name);
+    console.log(chars);
   } catch(error){
     console.log(error)
   }
