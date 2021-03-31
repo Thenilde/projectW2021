b86773757b87368985b280208e29b13ce6161e3c
Thenikde <thenilde.tb@hotmail.com>
2021-03-31 01:18:23 +0000

    changes22

--- item-controller.js
+++ item-controller.js
@@ -2,15 +2,13 @@ exports.getWorld = function(req,res){
     res.json({result: 'Hello Wrold from Controller'});
 }
 
- exports.getWorldParams=function(req ,res){
-    res.json({message :'Hello Thenilde Borges' , data:[
+exports.getWorldParams = function(req,res){
+    res.json({message: 'Hello BScBest!', data: [
         req.params.foo,
         req.params.bar
-
     ]});
-    
- };
+};
 
- exports.postWorld=function(req ,res){
+exports.postWorld = function(req,res){
     res.json({result: 'Post was sent', data: req.body});
- };
+};
--- routes.js
+++ routes.js
@@ -1,12 +1,19 @@
-const express=require('express'),
-router =express.Router();
+const express = require('express'),
+router = express.Router();
 
-var itemCtrl = require('./item-controller');
+var itemCtrl = require('./item-controller'),
+userCtrl = require('./user-controller');
 
 router.get('/hello', itemCtrl.getWorld);
 
-router.get('/hello/:foo/:bar' ,itemCtrl.getWorldParams);
+router.get('/hello/:foo/:bar', itemCtrl.getWorldParams);
 
-router.post('/hello' ,itemCtrl.postWorld);
+router.post('/hello', itemCtrl.postWorld);
 
-module.exports =router;
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers);
+router.get('/users/:id', userCtrl.getUser);
+router.put('/users/:id', userCtrl.updateUser);
+router.delete('/users/:id', userCtrl.deleteUser);
+
+module.exports = router;
