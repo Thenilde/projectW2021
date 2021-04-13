629862b330d87fb9bae3266686db2a15ce773f22
Thenikde <thenilde.tb@hotmail.com>
2021-04-13 09:28:52 +0000

    chaanges100

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const http = require('http'),
-axios = require('axios'),
+// axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
 bodyParser = require('body-parser'),
-mongoose = require('mongoose');//Allow us to connect to db.
+mongoose = require('mongoose');
 
-var app = express(); //express middleware
-var port = 8000;//server
-
+var app = express();
+var port = 8000;
 
 app.use(bodyParser.json());
 app.use(logger('tiny'));
@@ -53,4 +52,4 @@ const dbURI = "mongodb://localhost/test";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
         .then((result) => console.log('connected to db'))
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
